eab8d10e0b73071d97c43bbfdc856d43a48c2267
pizza <pizza@test.com>
2026-09-03 12:27:24 +0800

    chore: 移除逆地理编码接口（定位功能下线，改为手动输入出发地）

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common'
 import { PlanModule } from './plan/plan.module'
 import { AuthModule } from './auth/auth.module'
-import { GeoController } from './geo/geo.controller'
 
 @Module({
-  imports: [PlanModule, AuthModule],
-  controllers: [GeoController]
+  imports: [PlanModule, AuthModule]
 })
 export class AppModule {}
